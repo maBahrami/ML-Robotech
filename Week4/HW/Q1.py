2428bf5d899dda6f9b99c50782ba6af1a16dbb67
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-26 09:52:44 +0330

    Done

--- Week4/HW/Q1.py
+++ Week4/HW/Q1.py
@@ -27,50 +27,101 @@ def allOneHot(train, test):
     features = ["menopause", "tumor-size", "inv-nodes", "node-caps",
                 "deg-malig", "breast", "breast-quad", "irradiat"]
 
-    encOneHot = OneHotEncoder(sparse_output=False)
-    trainX = encOneHot.fit_transform(np.array(train["age"]).reshape(-1, 1))
+    enc = OneHotEncoder(sparse_output=False)
+    trainX = enc.fit_transform(np.array(train["age"]).reshape(-1, 1))
     #print(encOneHot.get_feature_names_out(["age"]))
     #print(trainOneHot.shape)
-    testX = encOneHot.transform(np.array(test["age"]).reshape(-1, 1))
+    testX = enc.transform(np.array(test["age"]).reshape(-1, 1))
 
     for i in features:
-        trainX = np.hstack([trainX, encOneHot.fit_transform(np.array(train[i]).reshape(-1, 1))])
+        trainX = np.hstack([trainX, enc.fit_transform(np.array(train[i]).reshape(-1, 1))])
         #print(i, trainOneHot.shape)
         #print(encOneHot.get_feature_names_out([i]))
-        testX = np.hstack([testX, encOneHot.transform(np.array(test[i]).reshape(-1, 1))])
+        testX = np.hstack([testX, enc.transform(np.array(test[i]).reshape(-1, 1))])
 
     return trainX, testX
+
 
 def allOrdinal(train, test):
     features = ["menopause", "tumor-size", "inv-nodes", "node-caps",
                 "deg-malig", "breast", "breast-quad", "irradiat"]
 
-    encOneHot = OrdinalEncoder()
-    trainX = encOneHot.fit_transform(np.array(train["age"]).reshape(-1, 1))
+    enc = OrdinalEncoder()
+    trainX = enc.fit_transform(np.array(train["age"]).reshape(-1, 1))
     #print(encOneHot.get_feature_names_out(["age"]))
     #print(trainOneHot.shape)
-    testX = encOneHot.transform(np.array(test["age"]).reshape(-1, 1))
+    testX = enc.transform(np.array(test["age"]).reshape(-1, 1))
 
     for i in features:
-        trainX = np.hstack([trainX, encOneHot.fit_transform(np.array(train[i]).reshape(-1, 1))])
+        trainX = np.hstack([trainX, enc.fit_transform(np.array(train[i]).reshape(-1, 1))])
         #print(i, trainOneHot.shape)
         #print(encOneHot.get_feature_names_out([i]))
-        testX = np.hstack([testX, encOneHot.transform(np.array(test[i]).reshape(-1, 1))])
+        testX = np.hstack([testX, enc.transform(np.array(test[i]).reshape(-1, 1))])
 
     return trainX, testX
+
+
+
+
+
+def OneHot_Ordinal(train, test):
+
+    ordinalFeatures = ["tumor-size", "inv-nodes", "deg-malig"]
+    nominalFeatures = ["menopause", "node-caps", "breast", "breast-quad", "irradiat"]
+    
+
+    enc = OrdinalEncoder()
+    trainX = enc.fit_transform(np.array(train["age"]).reshape(-1, 1))
+    #print(enc.get_feature_names_out(["age"]))
+    #print(trainOneHot.shape)
+    testX = enc.transform(np.array(test["age"]).reshape(-1, 1))
+    #print(testX[0:10])
+
+    for i in ordinalFeatures:
+        trainX = np.hstack([trainX, enc.fit_transform(np.array(train[i]).reshape(-1, 1))])
+        #print(i, trainOneHot.shape)
+        #print(enc.get_feature_names_out([i]))
+        testX = np.hstack([testX, enc.transform(np.array(test[i]).reshape(-1, 1))])
+        #print(testX[0:10])
+
+    #print(testX.shape)
+
+    enc2 = OneHotEncoder(sparse_output=False)
+    for i in nominalFeatures:
+        trainX = np.hstack([trainX, enc2.fit_transform(np.array(train[i]).reshape(-1, 1))])
+        #print(i, trainOneHot.shape)
+        #print(encOneHot.get_feature_names_out([i]))
+        testX = np.hstack([testX, enc2.transform(np.array(test[i]).reshape(-1, 1))])
+        #print(testX[0:10])
+
+    return trainX, testX
+
+
+
 
 
 
 
 
 # ----------------------------------------------------------------
-trainX, testX = allOrdinal(train, test)
+#trainX, testX = allOrdinal(train, test)
 
 #trainX, testX = allOneHot(train, test)
+
+trainX, testX = OneHot_Ordinal(train, test)
+
+#print(testX.shape)
 
 trainY = train["class"]
 testY = test["class"]
 
+# -------------------------- SGD Classifier ---------------------
+clf = SGDClassifier(loss="log_loss", tol=1e-5, random_state=123)
+clf.fit(trainX, trainY)
+
+y_pred = clf.predict(testX)
+acc = accuracy_score(y_pred, testY)
+#print(acc)
 
 # ------------------------ Logistic Regression ------------------
 clf = LogisticRegression()
@@ -80,3 +131,5 @@ y_pred = clf.predict(testX)
 
 acc = accuracy_score(y_pred, testY)
 print(acc)
+print(clf.coef_)
+
