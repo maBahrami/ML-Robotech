45cd66e70e0d7f1bd74930d86e12cc81a3575892
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-02-05 17:39:36 +0330

    done

---
+++ Week7/CAPTCHA_test.py
@@ -0,0 +1,50 @@
+import cv2
+from tensorflow.keras.models import load_model
+import numpy as np
+
+net = load_model(r"Week7\reference\CAPTCHA.h5")
+
+img = cv2.imread(r"Week7\reference\captcha_sample2.PNG")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#print(img.shape)
+#print(gray.shape)
+
+_, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+
+cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#print(cnts[0].shape)
+
+
+#cv2.drawContours(img, cnts, -1, (0, 255, 0), 1)
+
+for c in cnts:
+    x, y, w, h = cv2.boundingRect(c)
+    roi = img[y-5:y+h+5, x-5:x+w+5]
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+
+    roi = cv2.resize(roi, (32, 32)).flatten()
+    roi = roi / 255
+    roi = np.array([roi])
+
+    out = net.predict(roi)[0]
+    max_index = np.argmax(out) + 1
+    print(max_index)
+
+
+    cv2.rectangle(img, (x-5, y-5), (x+w+5, y+h+5), (0, 255, 0), 2)
+    cv2.putText(img, str(max_index), (x+2, y-10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (0, 0, 255), 1)
+
+
+    
+    #cv2.rectangle(img, (x-5, y-5), (x+w+5, y+h+5), (0, 255, 0), 2)
+
+cv2.imshow("image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+"""   
+cv2.imshow("image", roi)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
